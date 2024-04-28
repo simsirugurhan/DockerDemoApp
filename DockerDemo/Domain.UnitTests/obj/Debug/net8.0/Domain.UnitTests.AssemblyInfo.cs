97c4a90a838f1c31ba57d09686138bd6b4583beb
ugurhan117 <ugurhan117@gmail.com>
2024-04-28 15:07:16 +0300

    created docker compose yml file

--- DockerDemo/Domain.UnitTests/obj/Debug/net8.0/Domain.UnitTests.AssemblyInfo.cs
+++ DockerDemo/Domain.UnitTests/obj/Debug/net8.0/Domain.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354f0bb9526327fe4cd9737a73b5a949ed1604dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DockerDemo/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ DockerDemo/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354f0bb9526327fe4cd9737a73b5a949ed1604dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
